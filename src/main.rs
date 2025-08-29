1e3208253b8dfa9bb63c51e07e0219d3cdc61ef6
Resolmi <privateli@qq.com>
2025-08-29 23:27:18 +0800

    feat: process

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use mini_redis::{Connection, Frame, Result};
+use std::collections::HashMap;
+
+use mini_redis::cmd::{Get, Set};
+use mini_redis::{Command, Connection, Frame, Result};
 use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
@@ -6,12 +9,12 @@ async fn main() {
     let addr = "127.0.0.1:6379";
     let listener = TcpListener::bind(addr)
         .await
-        .expect("Mini-Redis bind {addr} error!");
+        .expect("Tiny-Redis bind {addr} error!");
 
-    println!("Mini-redis running on {addr}");
+    println!("Tiny-redis running on {addr}");
 
     loop {
-        let (tcp_stream, socket_addr) = listener.accept().await.expect("Mini-Redis Accept error!");
+        let (tcp_stream, socket_addr) = listener.accept().await.expect("Tiny-Redis Accept error!");
         println!("Conn Socket Addr: {socket_addr}");
 
         tokio::spawn(async {
@@ -23,7 +26,29 @@ async fn main() {
 }
 
 async fn process(tcp_stream: TcpStream) -> Result<()> {
+    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
+
     let mut conn: Connection = Connection::new(tcp_stream);
+    while let Some(frame) = conn.read_frame().await? {
+        let response = match Command::from_frame(frame)? {
+            Command::Get(get) => {
+                if let Some(val) = db.get(get.key()) {
+                    Frame::Bulk(val.clone().into())
+                } else {
+                    Frame::Null
+                }
+            }
+            Command::Set(set) => {
+                db.insert(set.key().to_string(), set.value().to_vec());
+                Frame::Simple("OK".into())
+            }
+            _ => Frame::Error("Unimplment".into()),
+        };
+
+        // 响应客户端
+        conn.write_frame(&response).await?;
+    }
+
     if let Some(frame) = conn.read_frame().await? {
         println!("Get Frame: {:?}", frame);
         let resp = Frame::Error("unimpl".into());
