2ce83ca7c3236ff402eee04ed529c8d247811501
resolmi <privateli@qq.com>
2025-08-30 00:41:40 +0800

    feat: lock

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    fmt::format,
+    sync::{Arc, Mutex},
+};
 
-use mini_redis::cmd::{Get, Set};
+use bytes::Bytes;
 use mini_redis::{Command, Connection, Frame, Result};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
@@ -11,38 +17,47 @@ async fn main() {
         .await
         .expect("Tiny-Redis bind {addr} error!");
 
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     println!("Tiny-redis running on {addr}");
 
     loop {
         let (tcp_stream, socket_addr) = listener.accept().await.expect("Tiny-Redis Accept error!");
         println!("Conn Socket Addr: {socket_addr}");
-
+        let db = db.clone();
+        // 交给 tokio runtime
         tokio::spawn(async {
-            if let Err(e) = process(tcp_stream).await {
+            if let Err(e) = process(tcp_stream, db).await {
                 println!("Connection failed: {}", e);
             }
         });
     }
 }
 
-async fn process(tcp_stream: TcpStream) -> Result<()> {
-    let mut db: HashMap<String, Vec<u8>> = HashMap::new();
-
+async fn process(tcp_stream: TcpStream, db: Db) -> Result<()> {
     let mut conn: Connection = Connection::new(tcp_stream);
     while let Some(frame) = conn.read_frame().await? {
         let response = match Command::from_frame(frame)? {
             Command::Get(get) => {
-                if let Some(val) = db.get(get.key()) {
-                    Frame::Bulk(val.clone().into())
+                if let Ok(db) = db.lock() {
+                    if let Some(val) = db.get(get.key()) {
+                        Frame::Bulk(val.clone().into())
+                    } else {
+                        Frame::Null
+                    }
                 } else {
-                    Frame::Null
+                    Frame::Simple("Error".into())
                 }
             }
             Command::Set(set) => {
-                db.insert(set.key().to_string(), set.value().to_vec());
-                Frame::Simple("OK".into())
+                if let Ok(mut db) = db.lock() {
+                    db.insert(set.key().to_string(), set.value().clone());
+                    Frame::Simple("OK".into())
+                } else {
+                    Frame::Simple("Error".into())
+                }
             }
-            _ => Frame::Error("Unimplment".into()),
+            other_cmd => Frame::Error(format!("Unimplment: {:?}", other_cmd)),
         };
 
         // 响应客户端
